176895d27a0db0804dd6b0b67039844e9c41e83d
jeayoon.hwang <jeayoon.hwang@h1-radio.com>
2025-11-07 15:13:43 +0900

    2nd

--- epollServer.c
+++ epollServer.c
@@ -32,4 +32,80 @@ int main(int argc, char **argv){
     setnonblocking(ssock);
     memset(&servaddr,0,sizeof(servaddr));
     servaddr.sin_family=AF_INET;
+    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    servaddr.sin_port=htons(SERVER_PORT);
+    if(bind(ssock, (struct sockaddr*)&servaddr, sizeof(servaddr))<0){
+        perror("bind()");
+        return -1;
+    }
+    setnonblocking(ssock);
+
+    memset(&servaddr,0,sizeof(servaddr));
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    servaddr.sin_port=htons(SERVER_PORT);
+    if(bind(ssock,(struct sockaddr*)&servaddr,sizeof(servaddr))<0){
+        perror("bind()");
+        return -1;
+    }
+    if(listen(ssock,8)<0){
+        perror("listen()");
+        return -1;
+    }
+
+    /* epoll_create() 함수를 이용해서 커널에 등록 */
+    epfd=epoll_create(MAX_EVENT);
+    if(epfd==-1){
+        perror("epoll_create()");
+        return 1;
+    }
+    /* epoll_ctl() 함수를 통해 감시하고 싶은 서버 소켓을 등록 */
+    ev.events=EPOLLIN;
+    ev.data.fd=ssock;
+
+    if(epoll_ctl(epfd,EPOLL_CTL_ADD,ssock,&ev)==-1){
+        perror("epoll_ctl()");
+        return 1;
+    }
+
+    do{
+        epoll_wait(epfd,events,MAX_EVENT,500);
+        for(int i=0;i<nfds;i++){
+            if(events[i].data.fd==ssock){
+                /* 읽기 가능한 소켓이 서버 소켓인 경우 */
+                clen=sizeof(struct sockaddr_in);
+                /* 클라이언트의 요청 받아들이기 */
+                csock=accept(ssock,(struct sockaddr*)&cliaddr,&clen);
+                if(csock>0){
+                    /* 네트워크 주소를 문자열로 변경 */
+                    inet_ntop(AF_INET,&cliaddr.sin_addr,mesg,BUFSIZ);
+                    printf("Clinet is connected: %s\n",mesg);
+
+                    setnonblocking(csock); // 클라이언트를 nonblocking mode로 설정
+                    
+                    /* 새로 접속한 클라이언트의 소켓 번호를 fd_set에 추가 */
+                    ev.events=EPOLLIN | EPOLLET;
+                    ev.data.fd=csock;
+
+                    epoll_ctl(epfd,EPOLL_CTL_ADD,csock,&ev);
+                    nfds++;
+                    continue;
+                }
+            }else if(events[i].events&EPOLLIN){
+                /* 클라이언트의 입력 */
+                if(events[i].data.fd<0) continue; // 소켓이 아닌 경우의 처리
+            memset(mesg,0,sizeof(mesg));
+                /* 해당 클라이언트에서 메시지를 읽고 다시 전송(Echo) */
+                if((n=read(events[i].data.fd,mesg,sizeof(mesg)))>0){
+                    printf("Received data: %s",mesg);
+                    write(events[i].data.fd,mesg,n);
+                    close(events[i].data.fd);
+                    epoll_ctl(epfd,EPOLL_CTL_DEL,events[i].data.fd,NULL);
+                    nfds--;
+                }
+            }
+        }
+    }while(strncmp(mesg,"q",1));
+    close(ssock);
+    return 0;
 }
