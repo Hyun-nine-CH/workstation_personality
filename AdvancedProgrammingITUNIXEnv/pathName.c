5e0fe1ca5b4e1a7410dd09fc468aaf98e989fc31
hyeongu choi <hyeongu.choi@h1-radio.com>
2025-12-02 17:18:18 +0900

    dynamics_1

--- AdvancedProgrammingITUNIXEnv/pathName.c
+++ AdvancedProgrammingITUNIXEnv/pathName.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]){
         err_quit("usage: ls directory_name"); // printf(stderr) + exit(1)
     if((dp=opendir(argv[1]))==NULL)
         err_sys("can't open%s",argv[1]); // err_sys("arg%s",errno), errno=argv[1]
-        // 기본적으로 err_*()은 exit(1) 포함
+        // commonly, err_*() does include exit(1)
     while((dirp=readdir(dp))!=NULL)
         printf("%s\n",dirp->d_name);
 
