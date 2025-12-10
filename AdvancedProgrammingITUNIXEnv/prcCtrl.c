9a91c0a994b7bdab318051a1d66b2455007156ef
hyeongu choi <hyeongu.choi@h1-radio.com>
2025-12-10 18:00:03 +0900

    messages_exec programs

--- AdvancedProgrammingITUNIXEnv/prcCtrl.c
+++ AdvancedProgrammingITUNIXEnv/prcCtrl.c
@@ -1,6 +1,7 @@
 #include "apue.h" // MAXLINE DEF
 #include <sys/wait.h>
 
+/*
 int main(void){
     char                buf[MAXLINE];
     pid_t               pid;
@@ -24,4 +25,40 @@ int main(void){
         printf("%% ");
     }
     exit(0);
+}
+*/
+int main(void){
+	/* type definitions */
+	int                 status;
+	pid_t                  pid;
+	char          buf[MAXLINE];
+	
+	/* ~% bash */
+	printf("%% ");
+	
+	/* launcher of input words */
+	while(fgets(buf,MAXLINE,stdin)){ // return: pointer; if failure, NULL
+		char *p=strchr(buf,'\n');
+		if(p)
+			*p='\0';
+	
+		/* process embranchments */
+		/* failure/error */
+		if((pid=fork())<0){
+			err_sys("fork error");
+		
+		}else if(pid==0){
+			execlp(buf,buf,(char*)0);
+			err_ret("~%: %s",buf);
+			_exit(127);
+		}
+		
+		if((pid=waitpid(pid,&status,0))<0)
+			err_sys("waitpid error");
+		
+		printf("%% ");
+	}
+	exit(0);
+	
+
 }
---
+++ AdvancedProgrammingITUNIXEnv/signal.c
@@ -0,0 +1,45 @@
+#include "apue.h"
+#include <sys/wait.h>
+
+/* signal capture start */
+static void sig_int(int);
+
+int main(void){
+    typedef struct S{
+        char         buf[MAXLINE];
+    } S;
+    
+    pid_t            pid;
+    int           status;
+    S s;
+    S *st=&s;
+    
+    /* signal error detection */
+    if(signal(SIGINT,sig_int)==SIG_ERR)
+        err_sys("signal error");
+
+    /* replay */
+    printf("%% ");
+    while((fgets(st->buf,MAXLINE,stdin))){
+        if(st->buf[strlen(st->buf)-1]=='\n')
+            st->buf[strlen(st->buf)-1]=0;
+
+        /* process embranchments */
+        if((pid=fork())<0){
+            err_sys("fork error emerges!!!!"); // error detection
+        }else if(pid==0){
+            /* implements */
+            execlp(st->buf,st->buf,(char*)0);
+            err_ret("can't execute: %s",st->buf);
+            _exit(127);
+        }
+        if((pid=waitpid(pid,&status,0))<0)
+            err_sys("waitpid error emerges!");
+        
+        printf("%% "); // loop bash print out
+    }
+    exit(0);
+}
+void sig_int(int signo){
+    printf("interrupt\n%% ");
+}
